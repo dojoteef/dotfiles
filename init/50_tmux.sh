c71a5b645c8a4703433714f66bf4b0df43390874
Nader Akoury <dojoteef@gmail.com>
2017-01-02 17:09:45 -0800

    Fix git shallow pull of tmux plugin manager

--- init/50_tmux.sh
+++ init/50_tmux.sh
@@ -9,7 +9,7 @@ else
   e_header "Updating tpm (tmux plugin manager)"
   cd "$tmux_plugins" || \
     (e_error "Unable to cd into $tmux_plugins. Possibly check permissions." && exit 1)
-  git pull --depth 1
+  git fetch origin master --depth 1 && git checkout -B master origin/master
   "$tmux_plugins/bin/clean_plugins" # remove plugins no longer in the list
   "$tmux_plugins/bin/install_plugins" # install new plugins
   "$tmux_plugins/bin/update_plugins" all # update existing plugins
