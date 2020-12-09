ce2687a0c206ee7b430015bff71b253229497cc6
Nader Akoury <dojoteef@gmail.com>
2020-12-09 07:47:10 -0800

    Slight correction

--- init/50_nvim.sh
+++ init/50_nvim.sh
@@ -42,8 +42,8 @@ if [[ "$(type -P nvim)" ]]; then
   e_header "Ensuring latest neovim packages for python3 are installed"
   # pip3 -q install --upgrade pynvim
   # See https://github.com/mhinz/neovim-remote/issues/134#issuecomment-586787280
-  pip3 -q install pynvim == 0.3.2
-  pip3 -q install --upgrade neovim-remote
+  python3 -m pip -q install pynvim==0.3.2
+  python3 -m pip -q install --upgrade neovim-remote
 fi
 
 ###########################################################################
