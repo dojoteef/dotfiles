89df63af288d15eb2bd7178f1ee6e8a28b1594c3
Nader Akoury <dojoteef@gmail.com>
2021-01-19 09:52:51 -0800

    Couple minor fixes to brew install

--- init/20_homebrew.sh
+++ init/20_homebrew.sh
@@ -3,11 +3,11 @@
 # Install Homebrew.
 if [[ ! "$(type -P brew)" ]]; then
   e_header "Installing Homebrew"
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Exit if, for some reason, Homebrew is not installed.
-[[ ! "$(type -P brew)" ]] && e_error "Homebrew failed to install." && return 1
+[[ ! "$(type -P brew)" ]] && e_error "Homebrew failed to install." && exit 1
 
 e_header "Updating Homebrew"
 brew doctor
