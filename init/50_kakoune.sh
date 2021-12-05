c449d0283c40e4f78407745283d50611bc0f1200
Nader Akoury <dojoteef@gmail.com>
2021-12-05 12:08:13 -0500

    Update kakoune support

--- init/50_kakoune.sh
+++ init/50_kakoune.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 
-# Ensure the .local/bin and .config/kak-lsp directories exist
-mkdir -p "$HOME/.local/bin"
+# Make sure the kakoune config directory exists
 mkdir -p "${XDG_CONFIG_HOME:=$HOME/.config}/kak"
-mkdir -p "${XDG_CONFIG_HOME:=$HOME/.config}/kak-lsp"
 
 # Hardcoded version of kak-lsp for now...
-if is_macos; then
-  TARBALL="kak-lsp-v8.0.0-x86_64-apple-darwin.tar.gz"
-elif is_linux; then
-  TARBALL="kak-lsp-v8.0.0-x86_64-unknown-linux-musl.tar.gz"
-fi
-URL="https://github.com/kak-lsp/kak-lsp/releases/download/v8.0.0/$TARBALL"
+if is_linux; then
+  URL="https://github.com/kak-lsp/kak-lsp/releases/download/v11.0.1/kak-lsp-v11.0.1-x86_64-unknown-linux-musl.tar.gz"
 
-cd "$HOME/.local/bin" && curl -fsSL "$URL" -o - | tar xzvf /dev/stdin kak-lsp
+  # Ensure .local/bin directories exist and install kak-lsp
+  mkdir -p "$HOME/.local/bin"
+  cd "$HOME/.local/bin" && curl -fsSL "$URL" -o - | tar xzvf /dev/stdin kak-lsp
+
+  KAK_LSP_CONFIG_DIR="${XDG_CONFIG_HOME:=$HOME/.config}/kak-lsp"
+elif is_macos; then
+  KAK_LSP_CONFIG_DIR="$HOME/Library/Preferences/kak-lsp"
+  [[ -e $HOME/Library/Preferences/kak-lsp/kak-lsp.toml ]] || ln -s "$DOTFILES/conf/kak-lsp.toml" "$XDG_CONFIG_HOME/kak-lsp/kak-lsp.toml"
+fi
+
+# Make sure the kak-lsp config directory exists
+mkdir -p "$KAK_LSP_CONFIG_DIR"
+
+# TODO: Override kak-lsp.toml if it exists? By default it'll exist if installed via brew, but it might be wrong...
+[[ -e $KAK_LSP_CONFIG_DIR/kak-lsp.toml ]] || ln -s "$DOTFILES/conf/kak-lsp.toml" "$KAK_LSP_CONFIG_DIR/kak-lsp.toml"
 [[ -e $XDG_CONFIG_HOME/kak/kakrc ]] || ln -s "$DOTFILES/conf/kakrc" "$XDG_CONFIG_HOME/kak/kakrc"
-[[ -e $XDG_CONFIG_HOME/kak-lsp/kak-lsp.toml ]] || ln -s "$DOTFILES/conf/kak-lsp.toml" "$XDG_CONFIG_HOME/kak-lsp/kak-lsp.toml"
