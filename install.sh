f30178f9289f92aa0f3ff499ce842b6241d88023
Nader Akoury <nsa@nsa-mbp.local>
2022-07-04 20:00:29 -0400

    more fixes...

--- install.sh
+++ install.sh
@@ -3,19 +3,19 @@
 set -e # -e: exit on error
 
 if [ ! "$(command -v chezmoi)" ]; then
-  bin_dir="$HOME/.local/bin"
-  chezmoi="$bin_dir/chezmoi"
+  BIN_DIR="$HOME/.local/bin"
+  CHEZMOI="$BIN_DIR/chezmoi"
   if [ "$(command -v curl)" ]; then
-    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
+    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$BIN_DIR"
   elif [ "$(command -v wget)" ]; then
-    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
+    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$BIN_DIR"
   else
     echo "To install chezmoi, you must have curl or wget installed." >&2
     exit 1
   fi
 else
-  chezmoi=chezmoi
+  CHEZMOI=chezmoi
 fi
 
 # exec: replace current process with chezmoi init
-exec "$chezmoi" init --apply dojoteef
+exec "$CHEZMOI" init --apply dojoteef
