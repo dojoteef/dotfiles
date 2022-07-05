66b17ab60abbefc20b4e36876034cfd6ad3d6d56
Nader Akoury <dojoteef@gmail.com>
2022-07-04 22:56:24 -0400

    fix the headers

--- home/.chezmoiscripts/run_before_00_script_utils.sh
+++ home/.chezmoiscripts/run_before_00_script_utils.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-cat <<EOF > ${TMPDIR-/tmp}/chezmoi-utils.sh
-e_header()   { echo -e "\n\033[1m$*\033[0m"; }
-e_success()  { echo -e " \033[1;32m✔\033[0m  $*"; }
-e_error()    { echo -e " \033[1;31m✖\033[0m  $*"; }
-e_arrow()    { echo -e " \033[1;34m➜\033[0m  $*"; }
+cat <<"EOF" > ${TMPDIR-/tmp}/chezmoi-utils.sh
+e_header()   { echo "\n\033[1m$*\033[0m"; }
+e_success()  { echo " \033[1;32m✔\033[0m  $*"; }
+e_error()    { echo " \033[1;31m✖\033[0m  $*"; }
+e_arrow()    { echo " \033[1;34m➜\033[0m  $*"; }
 
 export -f e_header
 export -f e_success
