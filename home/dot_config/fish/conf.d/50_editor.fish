ca499833a30b13c25b757daea8331bfa67cab4d7
Nader Akoury <dojoteef@gmail.com>
2022-09-23 11:07:38 -0400

    improve skim synctex support

--- home/dot_config/fish/conf.d/50_editor.fish
+++ home/dot_config/fish/conf.d/50_editor.fish
@@ -1,6 +1,10 @@
 if type -q kak
     set -gx EDITOR kak
     abbr -a kak-ide kak -e ide
+    # Requires Skim.app to have the following
+    # Command: sh
+    # Arguments: -c "echo 'evaluate-commands -verbatim -client %opt{jumpclient} -- edit -- %file %line' | kak -p synctex"
+    abbr -a kak-tex kak -e "'ide synctex'"
 else if type -q vim
     set -gx EDITOR vim
 end
